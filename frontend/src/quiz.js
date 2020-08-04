7dfce60cb02d36c05008c55a63abf779bc020bde
jzhuge99 <jennytzhuge@gmail.com>
2020-08-04 12:26:51 -0700

    updated quiz

---
+++ frontend/src/quiz.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+class Quiz extends Component  {
+    render() {
+        return(
+            <div>
+                //create a form tag here
+            </div>
+        )
+    }
+}
+
+export default Quiz
